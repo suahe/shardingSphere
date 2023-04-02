8b8c0a1fa608511fe2abc7da98f284b693e347a1
suahe <sssuahe@163.com>
2023-04-02 15:05:42 +0800

    fix:解决swagger报错问题

--- src/main/java/com/sah/shardingSphere/entity/SysUser.java
+++ src/main/java/com/sah/shardingSphere/entity/SysUser.java
@@ -84,6 +84,6 @@ public class SysUser implements Serializable {
 
     @Bit(digit = 3)
     @TableField(exist = false)
-    @ApiModelProperty(value = "double位数测试字段", example = "0.11")
+    @ApiModelProperty(value = "double位数测试字段")
     private Double num;
 }
